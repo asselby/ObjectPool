a04ce95f6cb955e84045a530908d7e88a44c8171
Асель Баймуханбетова <aselya119933@mail.ru>
2018-03-14 14:20:49 +0600

    obj pool first commit

---
+++ ObjectPool/Pool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPool
+{
+    public class Pool<T> where T : class, new()
+    {
+        public List<T> objectList = new List<T>();
+        public Pool( T obj, int maxValue)
+        {
+            for (int i = 0; i < maxValue; i++)
+            {
+                objectList.Add(Create());
+            }
+        }
+
+        public T GetObject()
+        {
+            if (objectList.Count == 0)
+            {
+                Console.WriteLine("Объектов в пуле нет");
+                return null;               
+            }
+                var obj = objectList.First();
+                objectList.Remove(obj);
+                return obj;
+        }
+
+        public void ReturnObject(T obj)
+        {
+            if (objectList.Count < 5)
+            {
+                objectList.Add(obj);
+            }
+            else  Console.WriteLine("Невозможно добавить объект в пул"); 
+        }
+
+        public T Create()
+        {
+            return new T();            
+        }
+
+
+    }
+}
---
+++ ObjectPool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pool<Taxi> poolTaxi = new Pool<Taxi>(new Taxi(), 5);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            var tax = poolTaxi.GetObject();
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.GetObject();
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.GetObject();
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.GetObject();
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.GetObject();
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            poolTaxi.ReturnObject(tax);
+            Console.WriteLine(poolTaxi.objectList.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
